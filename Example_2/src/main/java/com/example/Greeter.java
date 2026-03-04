5a91021296096070a68376498a40410d14f70e78
Justin Nguyen <jooton11@gmail.com>
2026-03-03 19:17:38 -0700

    Trigger pipeline

--- Example_2/src/main/java/com/example/Greeter.java
+++ Example_2/src/main/java/com/example/Greeter.java
@@ -1,15 +1,17 @@
 package com.example;
 
 public class Greeter {
-
+    // This method takes a name as input and returns a greeting message.
     public String greet(String name) {
         return "Hello, " + name + "!";
     }
 
+    // This method takes a name as input and returns a loud greeting message.
     public String greetLoud(String name) {
         return "HELLO, " + name.toUpperCase() + "!";
     }
 
+    // This method takes a name as input and returns a formal greeting message.
     public String greetFormal(String name) {
         return "Good day, " + name + ".";
     }
